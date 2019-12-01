680e934b53294eecf70435ee4d9b42a8a854f8a5
Matt Lewis <matt@m-lewis.co.uk>
2019-12-01 12:11:13 +0000

    Add basic phaser start page

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let win;
 
 const createWindow = () => {
 	win = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 814,
+		height: 657,
 		webPreferences: {
 			nodeIntegration: true,
 		},
